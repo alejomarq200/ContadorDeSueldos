1388d1de07bd77a44fc65e5fa3c18a1fd9d86e84
alejomarq200 <josemarq656@gmail.com>
2025-09-18 20:07:47 -0400

    Inicio de proyecto Estructuras básicas

---
+++ controlador/contolador_FormularioOrganizacion.php
@@ -0,0 +1,16 @@
+<?php
+// 0. Include de archivos necesarios
+
+// 1. Validamos REQUEST METHOD
+
+//2. Creación de array con valores obtenidos del formulario y uso de claves en array (si es necesario)
+
+//3. Flag, array de errores
+
+// 4. Validación de campos vacio y longitud mínima
+
+// 5. Si pasa flag crear en funciones tú función, incluir y usarla
+
+// 6. Uso de función con retorno para igualar a variable y comparar estado devuelto
+
+// 7. Evitar duplicado de datos por departamento
